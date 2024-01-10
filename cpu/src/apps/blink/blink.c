3606788a000fcb1dc15cd535d49ba7e36fdf4d9c
Bang Corrupt <bangcorrupt@gmail.com>
2024-01-10 18:36:19 +0000

    blink

---
+++ cpu/src/apps/blink/blink.c
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------
+
+                     This file is part of Freetribe
+
+                https://github.com/bangcorrupt/freetribe
+
+                                License
+
+                   GNU AFFERO GENERAL PUBLIC LICENSE
+                      Version 3, 19 November 2007
+
+                           AGPL-3.0-or-later
+
+ Freetribe is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+                  (at your option) any later version.
+
+     Freetribe is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty
+        of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+          See the GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+                       Copyright bangcorrupt 2023
+
+----------------------------------------------------------------------*/
+
+/**
+ * @file    blink.c
+ *
+ * @brief   Minimal example application for Freetribe.
+ *
+ * This simpe example uses a non-blocking delay 
+ * to toggle an LED at regular intervals.
+ */
+
+#include "freetribe.h"
+
+// 1 second in microseconds.
+#define DELAY_TIME 1000000 
+
+static uint32_t g_start_time; 
+
+t_status app_init(void) {
+
+    // Set start time.
+    g_start_time = ft_get_delay_current();
+
+    return SUCCESS;
+}
+
+void app_run(void) {
+
+    // Wait for delay.
+    if (ft_delay(g_start_time, DELAY_TIME)) {
+
+        // Toggle LED.
+        ft_toggle_led(LED_TAP);
+
+        // Reset start time.
+        g_start_time = ft_get_delay_current();
+
+    }
+}
+
+
