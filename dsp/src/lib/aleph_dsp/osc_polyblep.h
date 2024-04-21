1ec7987a8c7557efd41da71cd8543fc77344b390
bangcorrupt <bangcorrupt@gmail.com>
2024-04-21 19:28:18 +0100

    Add aleph-dsp submodule.

--- dsp/src/lib/aleph_dsp/osc_polyblep.h
+++
@@ -1,9 +0,0 @@
-#ifndef OSC_POLYBLEP_H
-#define OSC_POLYBLEP_H
-#include "types.h"
-
-fract16 saw_polyblep (fract32 p, fract32 dp);
-fract16 square_polyblep (fract32 p, fract32 dp);
-fract16 sine_polyblep (fract32 phase);
-fract16 triangle_polyblep (fract32 phase);
-#endif
