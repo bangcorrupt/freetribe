882cecfae5c531db2313d1f435357ced862f4b04
bangcorrupt <bangcorrupt@gmail.com>
2025-05-29 17:15:02 +0100

    feat(events): Enqueue/Dequeue events.
    Signed-off-by: bangcorrupt <bangcorrupt@gmail.com>

--- cpu/src/kernel/knl_events.c
+++ cpu/src/kernel/knl_events.c
@@ -29,24 +29,79 @@ under the terms of the GNU Affero General Public License as published by
 ----------------------------------------------------------------------*/
 
 /**
- * @file    template.c
+ * @file    knl_events.c
  *
- * @brief   Template for source files.
+ * @brief   Central event queue.
  */
 
 /*----- Includes -----------------------------------------------------*/
 
+#include "ring_buffer.h"
+
 /*----- Macros -------------------------------------------------------*/
+
+#define KNL_EVENT_QUEUE_LEN 0x200
+#define KNL_DATA_QUEUE_LEN 0x200
+#define KNL_EVENT_LEN sizeof(t_event)
 
 /*----- Typedefs -----------------------------------------------------*/
 
+typedef struct {
+    uint16_t id;
+    uint16_t len;
+    uint8_t *data;
+
+} t_event;
+
 /*----- Static variable definitions ----------------------------------*/
+
+// Event queue.
+static rbd_t g_event_rbd;
+static uint8_t g_event_rbmem[KNL_EVENT_QUEUE_LEN][KNL_EVENT_LEN];
+
+// Data queue.
+static rbd_t g_data_rbd;
+static uint8_t g_data_rbmem[KNL_DATA_QUEUE_LEN];
 
 /*----- Extern variable definitions ----------------------------------*/
 
 /*----- Static function prototypes -----------------------------------*/
 
 /*----- Extern function implementations ------------------------------*/
+
+void knl_event_enqueue(t_event *event) {
+
+    /// TODO: Test for queue put failure.
+    /// TODO: Test for data pointer errors.
+
+    ring_buffer_put(g_event_rbd, event);
+
+    if (event->len > 0 && event->data != NULL) {
+
+        uint8_t *data = event->data;
+
+        int i;
+        for (i = 0; i < event->len; i++) {
+
+            ring_buffer_put(g_data_rbd, data++);
+        }
+    }
+}
+
+t_status knl_event_dequeue(t_event *event) {
+
+    t_status result = ERROR;
+
+    if (ring_buffer_get(g_data_rbd, event) == SUCCESS) {
+
+        if (event->len > 0) {
+
+            uint8_t *data = malloc(event->len);
+        }
+    }
+
+    return result;
+}
 
 /*----- Static function implementations ------------------------------*/
 
